56652a6a2eaf872aed3ff7bc08b36725b18cfd80
hmerciol <li.henri@merciol.fr>
2018-01-05 15:32:30 +0100

    tp3 profiles

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,12 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("APP");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println( asciiArt );
+        
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString( "environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
